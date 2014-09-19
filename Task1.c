dca0410ab9290ca1bbe6f242c353f1fde26aaa46
KESHAmambo <a.baranok@yandex.ru>
2014-09-19 10:47:45 +0400

    Fixed incomplete solution, output, indents

--- Task1.c
+++ Task1.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 #include <math.h>
 int main(void) {
-double a=0, b=0, c=0;//int, float ,unsigned int,char(один символ)
+double a=0, b=0, c=0;
+printf("Please, enter coefficients a, b and c for quadratic equation");
 scanf("%lg %lg %lg",&a,&b,&c);
 if(b==0 && a==0){
- if(c==0){
-  printf("x-любое");
- else
-  printf("корней нет");
+    if(c==0)
+        printf("x- any real number");
+    else
+        printf("no solutions");
 }
 else if(a==0){
- double x=-c/b;
- printf("%lg",x);
+    double x=-c/b;
+    printf("%lg",x);
 }
 else if(b==0){
- if(c<0){
-  double x=sqtr(-c/a);
-  double y=-x;
-  printf("корни: %lg и %lg",x,y);
- }
- else if(c==0){
-  double x=0;
-  printf("один корень: %lg",x);
- }
- else
- printf"корней нет");
+    if(c<0){
+        double x=sqtr(-c/a);
+        double y=-x;
+        printf("two solutions: %lg and %lg",x,y);
+    }
+    else if(c==0){
+        double x=0;
+        printf("one solution: %lg",x);
+    }
+    else
+        printf"no solutions");
+}
+else if(c==0){
+    double x=-b/a;
+    double y=0;
+    printf("two solutions: %lg and %lg",x,y);
 else{
- double d=b*b-4*a*c;
- if(d>0){
-  double x=(-b+sqrt(d))/(2*a);
-  double y=(-b+sqrt(d))/(2*a);
-  printf("корни: %ld и %lg",x,y);
- }
- else if(d==0){
-  double x=-b/(2*a);
-  printf("один корень: %lg",x);
-  }
- else
-  printf("корней нет");
+    double d=b*b-4*a*c;
+    if(d>0){
+        double x=(-b+sqrt(d))/(2*a);
+        double y=(-b+sqrt(d))/(2*a);
+        printf("two solutions: %lg and %lg",x,y);
+    }
+    else if(d==0){
+        double x=-b/(2*a);
+        printf("one solution: %lg",x);
+    }
+    else
+    printf("no solutions");
 }
 
   return 0;
