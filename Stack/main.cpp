05d8bb15859669d5852374f27492225cf8d45023
KESHAmambo <a.baranok@yandex.ru>
2014-10-09 17:36:21 +0400

    Update main.cpp

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -3,34 +3,34 @@
 
 int main()
 {
- struct List* stack = NULL;
- if (isEmpty(stack))
-  printf_s("Stack is empty now.\n");
- else
-  printf_s("Stack includes something.\n");
- stack = (struct List*)calloc(1, sizeof(struct List));
- stack->value = 111;
- stack->next = NULL;
- stack->number = 1;
+    struct List* stack = NULL;
+    if (isEmpty(stack))
+        printf_s("Stack is empty now.\n");
+    else
+        printf_s("Stack includes something.\n");
+    stack = (struct List*)calloc(1, sizeof(struct List));
+    stack->value = 111;
+    stack->next = NULL;
+    stack->number = 1;
 
- stack = push(stack, 111);
- stack = push(stack, 222);
- stack = push(stack, 333);
- stack = push(stack, 444);
+    stack = push(stack, 111);
+    stack = push(stack, 222);
+    stack = push(stack, 333);
+    stack = push(stack, 444);
 
- if (isEmpty(stack))
-  printf_s("Stack is still empty.\n");
- else
- {
-  printf_s("We added some elements and now stack isn't empty:\n");
-  printf_s("%d\n", pop(stack));
-  printf_s("%d\n", getCount(stack));
- }
+    if (isEmpty(stack))
+        printf_s("Stack is still empty.\n");
+    else
+    {
+        printf_s("We added some elements and now stack isn't empty:\n");
+        printf_s("%d\n", pop(stack));
+        printf_s("%d\n", getCount(stack));
+    }
 
- stack = Delete(stack);
- printf_s("After deleting:\n");
- printf_s("%d\n", pop(stack));
- printf_s("%d\n", getCount(stack));
- getchar();
- return 0;
+    stack = Delete(stack);
+    printf_s("After deleting:\n");
+    printf_s("%d\n", pop(stack));
+    printf_s("%d\n", getCount(stack));
+    getchar();
+    return 0;
 }
