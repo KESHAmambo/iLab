aca3d2d7614e2b553f9c724b7f4365e0d877c4bb
KESHAmambo <a.baranok@yandex.ru>
2014-10-09 17:34:52 +0400

    Update stack.h

--- Stack/stack.h
+++ Stack/stack.h
@@ -4,15 +4,15 @@
 
 struct List
 {
- int value;
- struct List* next;
- int number;
+    int value;
+    struct List* next;
+    int number;
 
- List* push(List* stack, int i);
- int pop(List* stack);
- int getCount(List* stack);
- bool isEmpty(List* stack);
- List* Delete(List* stack);
+    List* push(List* stack, int i);
+    int pop(List* stack);
+    int getCount(List* stack);
+    bool isEmpty(List* stack);
+    List* Delete(List* stack);
 
 };
 
