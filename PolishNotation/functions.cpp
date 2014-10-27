83f3ae70fe361a77b1a157879fa7d4f9c2b3d4bf
KESHAmambo <a.baranok@yandex.ru>
2014-10-27 20:12:56 +0300

    Fixed inttoc()

--- PolishNotation/functions.cpp
+++ PolishNotation/functions.cpp
@@ -3,36 +3,36 @@
 
 int inttoc(char c)
 {
-	switch (c)
+	switch (c - '0')
 	{
-	case '0':
+	case 0:
 		return 0;
-	case '1':
+	case 1:
 		return 1;
-	case '2':
+	case 2:
 		return 2;
-	case '3':
+	case 3:
 		return 3;
-	case '4':
+	case 4:
 		return 4;
-	case '5':
+	case 5:
 		return 5;
-	case '6':
+	case 6:
 		return 6;
-	case '7':
+	case 7:
 		return 7;
-	case '8':
+	case 8:
 		return 8;
-	case '9':
+	case 9:
 		return 9;
-	case '+':
-		return 10;
-	case '-':
-		return 11;
-	case '*':
-		return 12;
-	case '/':
-		return 13;
+	case -5:
+		return -5;
+	case -3:
+		return -3;
+	case -6:
+		return -6;
+	case -1:
+		return -1;
 	}
 }
 
