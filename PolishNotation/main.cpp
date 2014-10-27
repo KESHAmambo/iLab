c3fa78925df153d747031a692d0017dee732f767
KESHAmambo <a.baranok@yandex.ru>
2014-10-27 20:14:32 +0300

    Removed unnecessary stacks

--- PolishNotation/main.cpp
+++ PolishNotation/main.cpp
@@ -4,14 +4,12 @@
 
 int main()
 {
-	
-
 	struct mystack stack;
 	printf("Please, enter 'F', when you will finish enter elements.\n");
 	char a = NULL;
 
 	
-	while (1) // entering polish notation
+	while (1) 
 	{
 		a = getchar();
 		if (a == 'F')
@@ -36,7 +34,7 @@ int main()
 	int d = 0;
 	int c = 0;
 
-		struct mystack stackV1;   // first number
+		struct mystack stackV;   // first number
 		while (1)
 		{
 			a = stackR.pop();
@@ -44,22 +42,20 @@ int main()
 				break;
 			else
 			{
-				stackV1.push(a);
+				stackV.push(a);
 			}
 		}
 
-		struct mystack stackVR1;
+		while (stackV.getNumberOfEl())
+			stack.push(stackV.pop());
 
-		while (stackV1.getNumberOfEl())
-			stackVR1.push(stackV1.pop());
-
-		while (stackVR1.getNumberOfEl())
+		while (stack.getNumberOfEl())
 		{
-			b = b + inttoc(stackVR1.pop()) * power(10, stackVR1.getNumberOfEl());
+			b = b + inttoc(stack.pop()) * power(10, stack.getNumberOfEl());
 		}
 
 		
-		struct mystack stackV2; // second number
+	    // second number
 		while (1)
 		{
 			a = stackR.pop();
@@ -67,34 +63,32 @@ int main()
 				break;
 			else
 			{
-				stackV2.push(a);
+				stackV.push(a);
 			}
 		}
 
-		struct mystack stackVR2;
+		while (stackV.getNumberOfEl())
+			stack.push(stackV.pop());
 
-		while (stackV2.getNumberOfEl())
-			stackVR2.push(stackV2.pop());
-
-		while (stackVR2.getNumberOfEl())
+		while (stack.getNumberOfEl())
 		{
-			d = d + inttoc(stackVR2.pop()) * power(10, stackVR2.getNumberOfEl());
+			d = d + inttoc(stack.pop()) * power(10, stack.getNumberOfEl());
 		}
 
 		c = inttoc(stackR.pop()); // numerical operation
 		switch (c)
 		{
-		  case 10:
-	  		b = b + d;
-	  		break;
-	  	case 11:
-	  		b = b - d;
-	  		break;
-	  	case 12:
-	  		b = b * d;
-	  		break;
-	  	case 13:
-	  		b = b / d;
+			case -5:
+				b = b + d;
+				break;
+			case -3:
+				b = b - d;
+				break;
+			case -6:
+				b = b * d;
+				break;
+			case -1:
+				b = b / d;
 		}
 
 		if (stackR.getNumberOfEl())
@@ -105,42 +99,41 @@ int main()
 		{
 			while (stackR.getNumberOfEl())   
 			{
-		  	struct mystack stackV3; // third number and further
-		  	while (1)
-		  	{
-		  		a = stackR.pop();
-		  		if (a == ' ' || a == 'F')
-	  				break;
-	  			else
-	  			{
-	  				stackV3.push(a);
-		  		}
+			    // third number and further
+				while (1)
+				{
+					a = stackR.pop();
+					if (a == ' ' || a == 'F')
+						break;
+					else
+					{
+						stackV.push(a);
+					}
 			}
 
-			struct mystack stackVR3;
 
-			while (stackV3.getNumberOfEl())
-				stackVR3.push(stackV3.pop());
+			while (stackV.getNumberOfEl())
+				stack.push(stackV.pop());
 
-			while (stackVR3.getNumberOfEl())
+			while (stack.getNumberOfEl())
 			{
-				d = d + inttoc(stackVR3.pop()) * power(10, stackVR3.getNumberOfEl());
+				d = d + inttoc(stack.pop()) * power(10, stack.getNumberOfEl());
 			}
 
 			c = inttoc(stackR.pop()); // numerical operation
 			switch (c)
 			{
-		  	case 10:
-		  		b = b + d;
-	  			break;
-	  		case 11:
-		  		b = b - d;
-	  			break;
-	  		case 12:
-		  		b = b * d;
-		  		break;
-	  		case 13:
-		  		b = b / d;
+				case -5:
+					b = b + d;
+					break;
+				case -3:
+					b = b - d;
+					break;
+				case -6:
+					b = b * d;
+					break;
+				case -1:
+					b = b / d;
 			}
 
 			if (stackR.getNumberOfEl())
