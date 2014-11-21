4dd0a26aad5c96fa330e6df7751b07dea33dc958
KESHAmambo <a.baranok@yandex.ru>
2014-11-21 20:57:39 +0300

    fixed inttoc()

--- Processor/stack.cpp
+++ Processor/stack.cpp
@@ -65,37 +65,7 @@ List* DeleteAll(List* stack)
 
 int inttoc(char c)
 {
-	switch (c - '0')
-	{
-	case 0:
-		return 0;
-	case 1:
-		return 1;
-	case 2:
-		return 2;
-	case 3:
-		return 3;
-	case 4:
-		return 4;
-	case 5:
-		return 5;
-	case 6:
-		return 6;
-	case 7:
-		return 7;
-	case 8:
-		return 8;
-	case 9:
-		return 9;
-	case -5:
-		return -5;
-	case -3:
-		return -3;
-	case -6:
-		return -6;
-	case -1:
-		return -1;
-	}
+	return (c - '0');
 }
 
 char add[20] = "add";
